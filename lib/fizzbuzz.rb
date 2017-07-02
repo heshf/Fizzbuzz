01ff0b55d0b8631948ddb02dd4dff62e35b54c50
Hesh <heshamf@gmail.com>
2017-07-02 13:23:14 +0100

    passed fifth test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,11 @@
 class Integer
 
 	def fizzbuzz
+		return 0 if self == 0
 		return "fizzbuzz" if self %15==0 #using 15 to cover multiples of BOTH 3 and 5
 		return "fizz" if self %3==0
 		return "buzz" if self %5==0
+
 		self
 	end
 
